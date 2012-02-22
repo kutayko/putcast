f43bb71e1c3f4c2215356ac6f6fceb6ead6934f3
Kutay Kocayurt <kutay.kocayurt@gmail.com>
2012-02-22 22:42:39 +0200

    easy querying

--- app.py
+++ app.py
@@ -16,13 +16,23 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
 @app.before_request
 def before_request():
     g.db = connect_db()
 
+
 @app.teardown_request
 def teardown_request(exception):
     g.db.close()
+
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+
 
 @app.route('/')
 def hello_world():
