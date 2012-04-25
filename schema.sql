a1ea3c57fa5ce88bf69a6900d176afd85b6dde53
Kutay Kocayurt <kutay.kocayurt@gmail.com>
2012-04-26 02:33:05 +0300

    schema

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 drop table if exists users;
 drop table if exists items;
 drop table if exists feeds;
+
 create table users (
   id integer primary key autoincrement,
   token string not null
@@ -16,6 +17,7 @@ create table feeds (
     id integer primary key autoincrement,
     user_id integer not null,
     name varchar not null,
+    token varchar not null,
     audio boolean default 1,
     video boolean default 0
 );
