493dc8f71f68247ce8996815031e310073c1bea0
Kutay Kocayurt <kutay.kocayurt@gmail.com>
2012-04-28 19:11:27 +0300

    new schema

--- schema.sql
+++ schema.sql
@@ -2,22 +2,17 @@ drop table if exists users;
 drop table if exists items;
 drop table if exists feeds;
 
-create table users (
-  id integer primary key autoincrement,
-  token string not null
-);
-
 create table items (
   id integer primary key autoincrement,
-  token varchar not null,
+  feed_token varchar not null,
   folder_id integer not null
 );
 
 create table feeds (
     id integer primary key autoincrement,
-    user_id integer not null,
+    user_token varchar not null,
     name varchar not null,
-    token varchar not null,
+    feed_token varchar not null,
     audio boolean default 1,
     video boolean default 0
 );
